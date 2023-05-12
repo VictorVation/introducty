197c2a9a1f047bd23709098fe8d62b05365f1d8b
Victor Szeto <v.victorszeto@gmail.com>
2023-05-12 18:06:39 -0400

    setup ui

--- app/(site)/setup/CopyLinkButton.tsx
+++ app/(site)/setup/CopyLinkButton.tsx
@@ -2,6 +2,7 @@
 
 import { ClipboardIcon } from "@heroicons/react/24/solid";
 import { toast } from "react-hot-toast";
+import { Button } from "~/components/ui/button";
 
 type Props = {
   username: string;
@@ -14,15 +15,18 @@ export default function CopyLinkButton({ username }: Props) {
       onClick={async (event) => {
         event.preventDefault();
         await navigator.clipboard.writeText(
-          `${process.env.NEXT_PUBLIC_BASE_URL}/${username}`
+          `https://${process.env.NEXT_PUBLIC_BASE_URL}/${username}`
         );
         toast.success("Link copied!", { id: "clipboard" });
       }}
     >
       <p className="mb-4 text-lg font-bold">{`${process.env.NEXT_PUBLIC_BASE_URL}/${username}`}</p>
-      <button className="mb-4 ml-4 flex rounded-md border bg-indigo-600 px-4 py-2 text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+      {/* <Button className="mb-4 ml-4 flex rounded-md border bg-brand-500 px-4 py-2 text-white shadow-sm hover:bg-brand-600 focus:outline-none focus:ring-2 focus:ring-offset-2">
         <ClipboardIcon className="h-6 w-6" /> Copy Link
-      </button>
+      </Button> */}
+      <Button>
+        <ClipboardIcon className="h-6 w-6" /> Copy Link
+      </Button>
     </div>
   );
 }
