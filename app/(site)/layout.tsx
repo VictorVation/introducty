9bda4292731423438b06098bdcb438bb41258dde
Victor Szeto <v.victorszeto@gmail.com>
2023-05-13 17:00:48 -0400

    Add user nav

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -1,9 +1,11 @@
+import { createServerComponentSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { cookies, headers } from "next/headers";
 import { notFound } from "next/navigation";
 
 import { MainNav } from "~/components/MainNav";
 import { SideNav } from "~/components/SideNav";
 import { SiteFooter } from "~/components/SiteFooter";
-// import { UserAccountNav } from "@/components/user-account-nav";
+import { UserAccountNav } from "~/components/UserAccountNav";
 
 interface DashboardLayoutProps {
   children?: React.ReactNode;
@@ -12,17 +14,21 @@ interface DashboardLayoutProps {
 export default async function DashboardLayout({
   children,
 }: DashboardLayoutProps) {
-  //   const user = await getCurrentUser();
-
-  //   if (!user) {
-  //     return notFound();
-  //   }
+  const supabase = createServerComponentSupabaseClient({ headers, cookies });
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+  const user = session?.user;
+  if (!user) {
+    return notFound();
+  }
 
   return (
     <div className="flex min-h-screen flex-col space-y-6">
       <header className="sticky top-0 z-40 border-b bg-background">
         <div className="container flex h-16 items-center justify-between py-4">
           <MainNav items={[]} />
+          <UserAccountNav user={{ name: user.email, email: user.email }} />
         </div>
       </header>
       <div className="container grid flex-1 gap-12 md:grid-cols-[200px_1fr]">
---
+++ components/UserAccountNav.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import Link from "next/link";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "~/components/ui/dropdown-menu";
+import { UserAvatar } from "./UserAvatar";
+import { useSupabase } from "~/app/supabase-provider";
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
+
+interface UserAccountNavProps {
+  user: { name?: string; email?: string };
+}
+
+export function UserAccountNav({ user }: UserAccountNavProps) {
+  const { supabase } = useSupabase();
+  const router = useRouter();
+
+  const { name, email } = user;
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <UserAvatar user={{ name: name ?? email ?? "" }} className="h-8 w-8" />
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <div className="flex items-center justify-start gap-2 p-2">
+          <div className="flex flex-col space-y-1 leading-none">
+            {name && <p className="font-medium">{name}</p>}
+            {email && (
+              <p className="w-[200px] truncate text-sm text-muted-foreground">
+                {email}
+              </p>
+            )}
+          </div>
+        </div>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem asChild>
+          <Link href="/dashboard">Dashboard</Link>
+        </DropdownMenuItem>
+        <DropdownMenuItem asChild>
+          <Link href="/dashboard/billing">Billing</Link>
+        </DropdownMenuItem>
+        <DropdownMenuItem asChild>
+          <Link href="/dashboard/settings">Settings</Link>
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem
+          className="cursor-pointer"
+          onSelect={async (event) => {
+            event.preventDefault();
+
+            await supabase.auth.signOut();
+            router.push("/signin");
+            toast("Signed out!");
+          }}
+        >
+          Sign out
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
