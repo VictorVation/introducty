a42a111ac9c0cea07f0ef9951ccb6f80087fc59d
Victor Szeto <v.victorszeto@gmail.com>
2023-05-18 00:43:21 -0400

    add branding

--- app/[siteName]/page.tsx
+++ app/[siteName]/page.tsx
@@ -3,6 +3,9 @@ import Link from "next/link";
 import { headers, cookies } from "next/headers";
 import { notFound } from "next/navigation";
 import { Database } from "~/types/supabase";
+import { SiteFooter } from "~/components/SiteFooter";
+import { Tags } from "lucide-react";
+import Logo from "~/components/Logo";
 
 type Props = {
   params: { siteName: string };
@@ -27,9 +30,9 @@ export default async function CreatorPage({ params: { siteName } }: Props) {
     ? [data.links]
     : [];
   return (
-    <div className={"mt-36 flex min-h-screen flex-col items-center"}>
+    <div className={"flex min-h-screen flex-col items-center p-24"}>
       <h1 className={"text-2xl font-bold"}>{siteName}</h1>
-      <div className="m-8 flex h-full w-96 flex-col justify-center gap-8 overflow-hidden">
+      <div className="m-8 flex h-full w-96 flex-col justify-center gap-4 overflow-hidden">
         {links.map((link) => (
           <Link
             className={"w-full rounded-lg border bg-white p-8 text-center"}
@@ -40,6 +43,19 @@ export default async function CreatorPage({ params: { siteName } }: Props) {
           </Link>
         ))}
       </div>
+      <footer className="mt-auto">
+        <div className="container bottom-0 mt-8 flex flex-col items-center gap-4 py-10 pb-16 text-sm md:h-24 md:flex-row md:py-0">
+          <div className="flex flex-col items-center gap-4 px-8 text-center md:flex-row md:gap-2 md:px-0">
+            <p className="text-md text-secondary-foreground sm:inline-block">
+              Powered by{" "}
+              <Link className="font-semibold underline" href="/">
+                Introducty
+              </Link>
+              .
+            </p>
+          </div>
+        </div>
+      </footer>
     </div>
   );
 }
