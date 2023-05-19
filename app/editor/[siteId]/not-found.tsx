e46d29b6011e5816b8c473512445d387969fcd2a
Victor Szeto <v.victorszeto@gmail.com>
2023-05-19 15:12:23 -0400

    update not found editor

--- app/editor/[siteId]/not-found.tsx
+++ app/editor/[siteId]/not-found.tsx
@@ -5,11 +5,11 @@ import { EmptyPlaceholder } from "~/components/EmptyPlaceholder";
 
 export default function NotFound() {
   return (
-    <EmptyPlaceholder className="mx-auto max-w-[800px]">
+    <EmptyPlaceholder className="mx-auto max-w-[800px] h-screen">
       <EmptyPlaceholder.Icon name="warning" />
       <EmptyPlaceholder.Title>Uh oh! Not Found</EmptyPlaceholder.Title>
       <EmptyPlaceholder.Description>
-        This post cound not be found. Please try again.
+        This site cound not be found. Please try again.
       </EmptyPlaceholder.Description>
       <Link
         href="/dashboard"
